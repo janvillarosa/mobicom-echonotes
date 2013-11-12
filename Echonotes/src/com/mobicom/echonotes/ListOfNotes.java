a0b0ac65fdf38b8527d68af87a56019ff30a02d3
janvillarosa <janrobertvillarosa@gmail.com>
2013-11-12 17:27:19 +0800

    Added UI for List of Echonotes
    -Added new theme "MyTheme" to change action bar color
    - Changed SQK version to 13 - 19
    - Added drawables for the list_notes.xml

--- Echonotes/src/com/mobicom/echonotes/ListOfNotes.java
+++ Echonotes/src/com/mobicom/echonotes/ListOfNotes.java
@@ -9,7 +9,7 @@ public class ListOfNotes extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_list_of_notes);
+		setContentView(R.layout.notes_list);
 	}
 
 	@Override
