dde568aa47acbd5f83ff3044f43abe9776a2eb24
janvillarosa <janrobertvillarosa@gmail.com>
2013-11-20 20:57:58 +0800

    Added Recording Functionality/Camera

--- Echonotes/src/com/mobicom/echonotes/RecordNote.java
+++ Echonotes/src/com/mobicom/echonotes/RecordNote.java
@@ -1,24 +1,154 @@
 package com.mobicom.echonotes;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class RecordNote extends Activity{
-	
+public class RecordNote extends Activity {
+
+	private static final String LOG_TAG = "AudioRecordTest";
+	private static String mFileName;
+	private MediaRecorder mRecorder = null;
+	private ImageView startRecord, newPhoto;
+	private boolean mStartRecording = true;
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	private Uri fileUri;
+	private static final int MEDIA_TYPE_IMAGE = 1;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.recorder_screen);
-		
+		startRecord = (ImageView) findViewById(R.id.startRecordImageView);
+		newPhoto = (ImageView) findViewById(R.id.newPhotoImageView);
+
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		// RECORD BUTTON LISTENER
+		startRecord.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				onRecord(mStartRecording);
+				mStartRecording = !mStartRecording;
+			}
+		});
 		
+		newPhoto.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent newPhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+				newPhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+				
+				startActivityForResult(newPhotoIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+			}
+		});
+
+
 	}
-	
+
+	private static Uri getOutputMediaFileUri(int type) {
+		return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(int type) {
+		// To be safe, you should check that the SDCard is mounted
+		// using Environment.getExternalStorageState() before doing this.
+
+		File mediaStorageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				"Echonotes");
+		// This location works best if you want the created images to be shared
+		// between applications and persist after your app has been uninstalled.
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d("Echonotes", "failed to create directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		File mediaFile;
+
+		if (type == MEDIA_TYPE_IMAGE) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "IMG_" + timeStamp + ".jpg");
+		} else {
+			return null;
+		}
+
+		return mediaFile;
+	}
+
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.recording_screen, menu);
 		return super.onCreateOptionsMenu(menu);
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) {
+				// Image captured and saved to fileUri specified in the Intent
+				Toast.makeText(this, "Image saved to:\n" + data.getData(),
+						Toast.LENGTH_LONG).show();
+			} else if (resultCode == RESULT_CANCELED) {
+				// User cancelled the image capture
+			} else {
+				// Image capture failed, advise user
+			}
+		}
+	}
+
+	private void onRecord(boolean start) {
+		if (start) {
+			startRecording();
+		} else {
+			stopRecording();
+		}
+	}
+
+	private void startRecording() {
+		mFileName = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+		mRecorder = new MediaRecorder();
+		mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+		mRecorder.setOutputFile(mFileName);
+		mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+		try {
+			mRecorder.prepare();
+		} catch (IOException e) {
+			Log.e(LOG_TAG, "prepare() failed");
+		}
+
+		mRecorder.start();
+	}
+
+	private void stopRecording() {
+		mRecorder.stop();
+		mRecorder.release();
+		mRecorder = null;
 	}
 
 }
